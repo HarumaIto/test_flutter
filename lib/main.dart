84283044a55ced3ecdde5722f3098e78831c43ec
HarumaIto <itoharm@gmail.com>
2023-12-06 18:47:04 +0900

    タスクのリスト化と日時選択ダイアログの追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,188 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List taskList = [
+    {
+      'title': 'Task 1',
+      'time': '10:00 AM',
+    },
+    {
+      'title': 'Task 2',
+      'time': '11:00 AM',
+    },
+    {
+      'title': 'Task 3',
+      'time': '12:00 AM',
+    },
+    {
+      'title': 'Task 4',
+      'time': '01:00 PM',
+    },
+    {
+      'title': 'Task 5',
+      'time': '02:00 PM',
+    },
+    {
+      'title': 'Task 6',
+      'time': '03:00 PM',
+    },
+    {
+      'title': 'Task 7',
+      'time': '04:00 PM',
+    },
+    {
+      'title': 'Task 8',
+      'time': '05:00 PM',
+    },
+    {
+      'title': 'Task 9',
+      'time': '06:00 PM',
+    },
+    {
+      'title': 'Task 10',
+      'time': '07:00 PM',
+    },
+    {
+      'title': 'Task 11',
+      'time': '08:00 PM',
+    },
+    {
+      'title': 'Task 12',
+      'time': '09:00 PM',
+    },
+    {
+      'title': 'Task 13',
+      'time': '10:00 PM',
+    },
+    {
+      'title': 'Task 14',
+      'time': '11:00 PM',
+    },
+    {
+      'title': 'Task 15',
+      'time': '12:00 PM',
+    },
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget taskCard(String title, String time) {
+    return Card(
+      margin: const EdgeInsets.all(16),
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(title),
+            Text(time),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Task'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.emoji_people))],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: taskList
+              .map((task) => taskCard(task['title'], task['time']))
+              .toList(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          DateTime? date;
+          TimeOfDay? time;
+          String dateText = '日付が選択されていません';
+          String timeText = '時間が選択されていません';
+
+          showDialog(
+              context: context,
+              builder: (context) {
+                return StatefulBuilder(builder: ((context, setState) {
+                  return SimpleDialog(
+                  title: const Text('タスクを追加'),
+                  children: [
+                    Text('日付: $dateText'),
+                    Text('時間: $timeText'),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () async {
+                        date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime.now().add(const Duration(days: 365)),
+                        );
+                        setState(() {
+                          if (date != null) {
+                            dateText = '${date!.year}年${date!.month}月${date!.day}日';
+                          }
+                        });
+                      },
+                      child: const Text('日付を選択'),
+                    ),
+                  
+                    ElevatedButton(
+                      onPressed: () async {
+                        time = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                        );
+                        setState(() {
+                          if (time != null) {
+                            timeText = '${time!.hour}時${time!.minute}分';
+                          }
+                        });
+                      },
+                      child: const Text('時間を選択'),
+                    ),
+                  ],
+                );
+                }));
+              });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Task'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Class'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Notice'),
+        ],
+      ),
     );
   }
 }
